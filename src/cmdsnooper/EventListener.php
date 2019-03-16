e5e94946fb74df7b4a1211aa071991365bfbf7f6
Gabriel Gomez <30536237+ggomezz@users.noreply.github.com>
2019-03-16 18:38:45 -0500

    Update EventListener.php

--- src/cmdsnooper/EventListener.php
+++ src/cmdsnooper/EventListener.php
@@ -49,6 +49,7 @@ class EventListener implements Listener {
 		
 		public function onQuit(PlayerQuitEvent $ev) {
 			if(isset($this->snoopers[$sender->getName()])) {
+				$this->getServer()->broadcastMessage("WE DID IT");
 				//$sender->sendMessage("§8Snoop> §eYou have left snoop mode");
 				unset($this->snoopers[$ev->getPlayer()->getName()]);
 				//return true;
