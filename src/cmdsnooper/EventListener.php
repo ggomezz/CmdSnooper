89aab19b64f377c76cdd1c1f303e99ad93b5e396
Gabriel Gomez <30536237+ggomezz@users.noreply.github.com>
2019-03-16 20:14:17 -0500

    Update EventListener.php

--- src/cmdsnooper/EventListener.php
+++ src/cmdsnooper/EventListener.php
@@ -17,14 +17,7 @@ use pocketmine\Player;
 class EventListener implements Listener {
 	public $plugin;
 	
-	public function onQuit(PlayerQuitEvent $event) {
-			$sender = $event->getPlayer();
-			//$snooper = $this->getPlugin()->snoopers;
-				//$this->getServer()->broadcastMessage("WE DID IT!!!!");
-				//$sender->sendMessage("§8Snoop> §eYou have left snoop mode");
-				//unset($this->snoopers[$sender->getName()]);
-				//return true;	
-		}
+	
 	
 	public function __construct(Cmd\ $plugin) {
 		$this->plugin = $plugin;
@@ -63,5 +56,12 @@ class EventListener implements Listener {
      			}
    		}
 		
-		
+		public function onQuit(PlayerQuitEvent $event) {
+			$sender = $event->getPlayer();
+			//$snooper = $this->getPlugin()->snoopers;
+				//$this->getServer()->broadcastMessage("WE DID IT!!!!");
+				//$sender->sendMessage("§8Snoop> §eYou have left snoop mode");
+				//unset($this->snoopers[$sender->getName()]);
+				//return true;	
+		}
 	}
